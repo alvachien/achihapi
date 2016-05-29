9b46acf29a63fa8ed6186ae2e69a0e945bd0144e
Alva Chien <alvachien@live.com>
2016-05-29 23:38:04 +0800

    Add the sqls

---
+++ src/achihapi/Sqls/DDL1.sql
@@ -0,0 +1,35 @@
+﻿USE [alvachiendb]
+GO
+
+CREATE TABLE [TodoItem] (
+    [ItemID] int NOT NULL IDENTITY,
+    [Name] nvarchar(50) NOT NULL,
+	[Content] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_TodoItem] PRIMARY KEY ([ItemId])
+);
+GO
+
+CREATE TABLE [EnPartOfSpeech] (
+	[POSID] int NOT NULL,
+	[POSName] nvarchar(50) NOT NULL,
+	CONSTRAINT [PK_EnPartOfSpeech] PRIMARY KEY ([POSID]),
+);
+GO
+
+CREATE TABLE [EnWord] (
+    [WordID] int NOT NULL IDENTITY,
+    [WordString] int NOT NULL,
+    [Tags] nvarchar(max),
+    CONSTRAINT [PK_EnWord] PRIMARY KEY ([WordID])
+);
+GO
+
+CREATE TABLE [EnWordExplain] (
+	[WordID] int NOT NULL,
+	[SeqID] int NOT NULL,
+	[POSID] int NOT NULL,
+	[Explain] nvarchar(200) NOT NULL
+    CONSTRAINT [PK_EnWordExplain] PRIMARY KEY ([WordID], [SeqID]),
+    CONSTRAINT [FK_EnWordExplain_WordID] FOREIGN KEY ([WordID]) REFERENCES [EnWord] ([WordID]) ON DELETE CASCADE
+);
+GO
