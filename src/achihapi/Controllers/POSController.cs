9994fc1925e9dba239e232de1724509158835dce
Alva Chien <alvachien@live.com>
2016-06-01 14:58:44 +0800

    Adding first controller

---
+++ src/achihapi/Controllers/POSController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using achihapi.ViewModels;
+
+namespace achihapi.Controllers
+{
+    [Route("api/[controller]")]
+    public class POSController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<POSViewModel> Get()
+        {
+            return null;
+        }
+    }
+}
---
+++ src/achihapi/ViewModels/POSViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace achihapi.ViewModels
+{
+    public class POSViewModel
+    {
+        public string POSAbb { get; set; }
+        public string POSName { get; set; }
+        public string LangID { get; set; }
+        public string POSNativeName { get; set; }
+    }
+}
