e3a6045bf47fd1548c1a94cd681ed71673f9c7dc
Alva Chien <alvachien@live.com>
2016-06-17 13:04:44 +0800

    Add Language part

---
+++ src/achihapi/Controllers/LanguageController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using achihapi.ViewModels;
+
+namespace achihapi.Controllers
+{
+    [Route("api/[controller]")]
+    public class LanguageController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<LanguageViewModel> Get()
+        {
+            List<LanguageViewModel> listCI = new List<LanguageViewModel>();
+            //CultureInfo ci = new CultureInfo("");
+            listCI.Add(new LanguageViewModel() { LCID = 4, Name="Chinese (Simplified)", NativeName="简体中文" });
+            listCI.Add(new LanguageViewModel() { LCID = 9, Name = "English", NativeName = "English" });
+            //listCI.Add(new LanguageViewModel() { LCID = 31748, Name = "Chinese (Traditional)", NativeName = "繁体中文" });
+            //CultureInfo[] cinfo = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            //foreach (CultureInfo cul in cinfo)
+            //{
+            //}
+            return listCI;
+        }
+    }
+}
---
+++ src/achihapi/ViewModels/LanguageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace achihapi.ViewModels
+{
+    public class LanguageViewModel
+    {
+        public Int32 LCID { get; set; }
+        public String Name { get; set; }
+        public String NativeName { get; set; }
+    }
+}
