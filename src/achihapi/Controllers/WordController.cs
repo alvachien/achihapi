fcc91cfe7e2411406a0e3a9fbcda4bc6599ff9d5
Alva Chien <alvachien@live.com>
2016-06-28 23:59:24 +0800

    Fix the 500 error on word loading

--- src/achihapi/Controllers/WordController.cs
+++ src/achihapi/Controllers/WordController.cs
@@ -27,20 +27,24 @@ namespace achihapi.Controllers
         {
             List<WordViewModel> listWords = new List<WordViewModel>();
             var poslist = from p1 in _dbContext.EnWord
-                          join p2 in _dbContext.EnWordExplain
+                          join  p2 in _dbContext.EnWordExplain
                             on p1.WordID equals p2.WordID 
                             into p12
 
                           from p3 in p12.DefaultIfEmpty()
                           join p4 in _dbContext.ENPOS on p3.POSAbb equals p4.POSAbb 
-                          join p6 in _dbContext.EnPOST on p4.POSAbb equals p6.POSAbb 
-                            into p14
+                            into p13
+                          
+                          from p5 in p13
+                          //join p6 in _dbContext.EnPOST on p5.POSAbb equals p6.POSAbb
+                          //  into p14
 
-                          from p7 in p14.DefaultIfEmpty()
+                          //from p7 in p14.DefaultIfEmpty()
                           join p8 in _dbContext.EnWordExplainT
-                            on new { p7.LangID, p3.WordID, p3.ExplainID }  equals new { p8.LangID, p8.WordID, p8.ExplainID }
+                            on new { p3.WordID, p3.ExplainID }  equals new { p8.WordID, p8.ExplainID }
+                          //where p8.LangID == lang
                           orderby p3.WordID, p3.ExplainID
-                          select new { p3.WordID, p3.ExplainID, p1.WordString, p1.Tags, p3.POSAbb, p7.LangID, p7.POSName, p8.ExplainString }                            
+                          select new { p3.WordID, p3.ExplainID, p1.WordString, p1.Tags, p3.POSAbb, p8.LangID, p5.POSName, p8.ExplainString }                            
                         ;
 
             foreach(var worddb in poslist)
