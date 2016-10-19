474c23de533d178911d77622a8e36cb99fe53a2b
Alva Chien <alvachien@163.com>
2016-10-19 16:57:38 +0800

    Add Language part

---
+++ src/achihapi/Controllers/LanguageController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using achihapi.ViewModels;
+
+namespace achihapi.Controllers
+{
+    [Route("api/[controller]")]
+    public class LanguageController : Controller
+    {
+        // GET: api/language
+        [HttpGet]
+        public IEnumerable<LanguageViewModel> Get()
+        {
+            List<LanguageViewModel> listVMs = new List<LanguageViewModel>();
+            SqlConnection conn = new SqlConnection(Startup.DBConnectionString);
+            String queryString = "";
+
+            try
+            {
+#if DEBUG
+                foreach (var clm in User.Claims.AsEnumerable())
+                {
+                    System.Diagnostics.Debug.WriteLine("Type = " + clm.Type + "; Value = " + clm.Value);
+                }
+#endif
+                var usrObj = User.FindFirst(c => c.Type == "sub");
+
+                queryString = @"SELECT TOP (1000) [LCID]
+                              ,[ISONAME]
+                              ,[ENNAME]
+                              ,[NAVNAME]
+                              ,[APPFLAG]
+                          FROM [achihdb].[dbo].[t_language]";
+
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(queryString, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        LanguageViewModel avm = new LanguageViewModel();
+                        avm.Lcid = reader.GetInt32(0);
+                        avm.ISOName = reader.GetString(1);
+                        avm.EnglishName = reader.GetString(2);
+                        avm.NativeName = reader.GetString(3);
+                        if (!reader.IsDBNull(4))
+                            avm.AppFlag = reader.GetBoolean(4);
+
+                        listVMs.Add(avm);
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                System.Diagnostics.Debug.WriteLine(exp.Message);
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+
+            return listVMs;
+        }
+
+        // GET api/language/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/language
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/language/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/language/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
