3e83c3847fe00bd877cf9a1d70af53515c18819d
Alva Chien <alvachien@163.com>
2016-10-21 11:44:40 +0800

    Add Read support to Finance Currency

--- src/achihapi/Controllers/FinanceCurrencyController.cs
+++ src/achihapi/Controllers/FinanceCurrencyController.cs
@@ -4,18 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using achihapi.ViewModels;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace achihapi.Controllers
 {
     [Route("api/[controller]")]
-    public class FinanceAccountCategoryController : Controller
+    public class FinanceCurrencyController : Controller
     {
-        // GET: api/financeaccountcateogry
+        // GET: api/financecurrency
         [HttpGet]
-        public IEnumerable<FinanceAccountCtgyViewModel> Get()
+        public IEnumerable<FinanceCurrencyViewModel> Get()
         {
-            List<FinanceAccountCtgyViewModel> listVMs = new List<FinanceAccountCtgyViewModel>();
+            List<FinanceCurrencyViewModel> listVMs = new List<FinanceCurrencyViewModel>();
             SqlConnection conn = new SqlConnection(Startup.DBConnectionString);
             String queryString = "";
 
@@ -29,16 +30,15 @@ namespace achihapi.Controllers
 #endif
                 var usrObj = User.FindFirst(c => c.Type == "sub");
 
-                queryString = @"SELECT TOP (1000) [ID]
-                          ,[NAME]
-                          ,[ASSETFLAG]
-                          ,[COMMENT]
-                          ,[SYSFLAG]
-                          ,[CREATEDBY]
-                          ,[CREATEDAT]
-                          ,[UPDATEDBY]
-                          ,[UPDATEDAT]
-                      FROM [achihdb].[dbo].[t_fin_account_ctgy]";
+                queryString = @"SELECT TOP (1000) [CURR]
+                              ,[NAME]
+                              ,[SYMBOL]
+                              ,[SYSFLAG]
+                              ,[CREATEDBY]
+                              ,[CREATEDAT]
+                              ,[UPDATEDBY]
+                              ,[UPDATEDAT]
+                          FROM [achihdb].[dbo].[t_fin_currency]";
 
                 conn.Open();
                 SqlCommand cmd = new SqlCommand(queryString, conn);
@@ -47,23 +47,21 @@ namespace achihapi.Controllers
                 {
                     while (reader.Read())
                     {
-                        FinanceAccountCtgyViewModel avm = new FinanceAccountCtgyViewModel();
-                        avm.ID = reader.GetInt32(0);
+                        FinanceCurrencyViewModel avm = new FinanceCurrencyViewModel();
+                        avm.Curr = reader.GetString(0);
                         avm.Name = reader.GetString(1);
                         if (!reader.IsDBNull(2))
-                            avm.AssetFlag = reader.GetBoolean(2);
+                            avm.Symbol = reader.GetString(2);
                         if (!reader.IsDBNull(3))
-                            avm.Comment = reader.GetString(3);
+                            avm.SysFlag = reader.GetBoolean(3);
                         if (!reader.IsDBNull(4))
-                            avm.SysFlag = reader.GetBoolean(4);
+                            avm.CreatedBy = reader.GetString(4);
                         if (!reader.IsDBNull(5))
-                            avm.CreatedBy = reader.GetString(5);
+                            avm.CreatedAt = reader.GetDateTime(5);
                         if (!reader.IsDBNull(6))
-                            avm.CreatedAt = reader.GetDateTime(6);
+                            avm.UpdatedBy = reader.GetString(6);
                         if (!reader.IsDBNull(7))
-                            avm.UpdatedBy = reader.GetString(7);
-                        if (!reader.IsDBNull(8))
-                            avm.UpdatedAt = reader.GetDateTime(8);
+                            avm.UpdatedAt = reader.GetDateTime(7);
 
                         listVMs.Add(avm);
                     }
@@ -82,26 +80,26 @@ namespace achihapi.Controllers
             return listVMs;
         }
 
-        // GET api/financeaccountcateogry/5
-        [HttpGet("{ id}")]
+        // GET api/financecurrency/5
+        [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/financeaccountcateogry
+        // POST api/financecurrency
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/financeaccountcateogry/5
+        // PUT api/financecurrency/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/financeaccountcateogry/5
+        // DELETE api/financecurrency/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
