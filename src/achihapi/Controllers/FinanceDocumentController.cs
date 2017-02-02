52bf47637c0f4b2ac5cb31a46c8739f544216615
Alva Chien <alvachien@163.com>
2017-02-02 13:48:53 +0800

    Add finance document controller, but it need improvement

---
+++ src/achihapi/Controllers/FinanceDocumentController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using achihapi.ViewModels;
+using System.Data.SqlClient;
+
+namespace achihapi.Controllers
+{
+    [Route("api/[controller]")]
+    public class FinanceDocumentController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        [Authorize]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        [Authorize]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        [Authorize]
+        public void Delete(int id)
+        {
+        }
+    }
+}
