a82e2ff6a83787fd1a7624365c7872375eacf376
Alva Chien <alvachien@163.com>
2017-02-02 23:07:03 +0800

    Add supporting for Order's creation

--- src/achihapi/Controllers/FinanceOrderController.cs
+++ src/achihapi/Controllers/FinanceOrderController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using achihapi.ViewModels;
+using System.Data;
 using System.Data.SqlClient;
 using Microsoft.AspNetCore.Authorization;
 
@@ -138,8 +139,185 @@ namespace achihapi.Controllers
 
         // POST api/financeorder
         [HttpPost]
-        public void Post([FromBody]string value)
+        [Authorize]
+        public async Task<IActionResult> Post([FromBody]FinanceOrderViewModel vm)
         {
+            if (vm == null)
+            {
+                return BadRequest("No data is inputted");
+            }
+
+            // Check on name
+            if (vm.Name != null)
+                vm.Name = vm.Name.Trim();
+            if (String.IsNullOrEmpty(vm.Name))
+            {
+                return BadRequest("Name is a must!");
+            }
+
+            // Check the s.rule
+            if (vm.SRuleList.Count <= 0)
+            {
+                return BadRequest("No rule has been assigned yet");
+            }
+            Int32 nPer = 0;
+            foreach(FinanceOrderSRuleUIViewModel svm in vm.SRuleList)
+            {
+                if (svm.Precent <= 0)
+                {
+                    return BadRequest("Percentage less than zero!");
+                }
+
+                nPer += svm.Precent;
+            }
+            if (nPer != 100 )
+            {
+                return BadRequest("Total percentage shall sum up to 100");
+            }
+
+            // ToDo: Check existence of the control center!!
+
+            // Update the database
+            SqlConnection conn = new SqlConnection(Startup.DBConnectionString);
+            String queryString = "";
+            Boolean bDuplicatedEntry = false;
+            Int32 nDuplicatedID = -1;
+            Int32 nNewID = -1;
+            Boolean bError = false;
+            String strErrMsg = "";
+
+            try
+            {
+#if DEBUG
+                foreach (var clm in User.Claims.AsEnumerable())
+                {
+                    System.Diagnostics.Debug.WriteLine("Type = " + clm.Type + "; Value = " + clm.Value);
+                }
+#endif
+                var usrName = User.FindFirst(c => c.Type == "sub").Value;
+
+                queryString = @"SELECT [ID]
+                  FROM [dbo].[t_fin_order] WHERE [Name] = N'" + vm.Name + "'";
+
+                await conn.OpenAsync();
+
+                SqlCommand cmd = new SqlCommand(queryString, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    bDuplicatedEntry = true;
+                    while (reader.Read())
+                    {
+                        nDuplicatedID = reader.GetInt32(0);
+                        break;
+                    }
+                }
+                else
+                {
+                    reader.Dispose();
+                    reader = null;
+
+                    cmd.Dispose();
+                    cmd = null;
+
+                    // Now go ahead for the creating
+                    SqlTransaction tran = conn.BeginTransaction();
+
+                    queryString = @"INSERT INTO [dbo].[t_fin_order]
+                               ([NAME]
+                               ,[VALID_FROM]
+                               ,[VALID_TO]
+                               ,[COMMENT]
+                               ,[CREATEDBY]
+                               ,[CREATEDAT]
+                               ,[UPDATEDBY]
+                               ,[UPDATEDAT])
+                         VALUES
+                               (@NAME
+                               ,@VALID_FROM
+                               ,@VALID_TO
+                               ,@COMMENT
+                               ,@CREATEDBY
+                               ,@CREATEDAT
+                               ,@UPDATEDBY
+                               ,@UPDATEDAT); SELECT @Identity = SCOPE_IDENTITY();";
+
+                    try
+                    {
+                        cmd = new SqlCommand(queryString, conn);
+                        cmd.Transaction = tran;
+                        cmd.Parameters.AddWithValue("@NAME", vm.Name);
+                        cmd.Parameters.AddWithValue("@VALID_FROM", vm.Valid_From);
+                        cmd.Parameters.AddWithValue("@VALID_TO", vm.Valid_To);
+                        cmd.Parameters.AddWithValue("@COMMENT", String.IsNullOrEmpty(vm.Comment) ? String.Empty : vm.Comment);
+                        cmd.Parameters.AddWithValue("@CREATEDBY", usrName);
+                        cmd.Parameters.AddWithValue("@CREATEDAT", vm.CreatedAt);
+                        cmd.Parameters.AddWithValue("@UPDATEDBY", DBNull.Value);
+                        cmd.Parameters.AddWithValue("@UPDATEDAT", DBNull.Value);
+                        SqlParameter idparam = cmd.Parameters.AddWithValue("@Identity", SqlDbType.Int);
+                        idparam.Direction = ParameterDirection.Output;
+
+                        Int32 nRst = await cmd.ExecuteNonQueryAsync();
+                        nNewID = (Int32)idparam.Value;
+
+                        // Then, creating the srules
+                        foreach (FinanceOrderSRuleUIViewModel suivm in vm.SRuleList)
+                        {
+                            queryString = @"INSERT INTO [dbo].[t_fin_order_srule]
+                                               ([ORDID]
+                                               ,[RULEID]
+                                               ,[CONTROLCENTERID]
+                                               ,[PRECENT]
+                                               ,[COMMENT])
+                                         VALUES
+                                               (@ORDID
+                                               ,@RULEID
+                                               ,@CONTROLCENTERID
+                                               ,@PRECENT
+                                               ,@COMMENT)";
+                            SqlCommand cmd2 = new SqlCommand(queryString, conn);
+                            cmd2.Transaction = tran;
+                            cmd2.Parameters.AddWithValue("@ORDID", nNewID);
+                            cmd2.Parameters.AddWithValue("@RULEID", suivm.RuleID);
+                            cmd2.Parameters.AddWithValue("@CONTROLCENTERID", suivm.ControlCenterID);
+                            cmd2.Parameters.AddWithValue("@PRECENT", suivm.Precent);
+                            cmd2.Parameters.AddWithValue("@COMMENT", String.IsNullOrEmpty(suivm.Comment) ? String.Empty : suivm.Comment);
+                            await cmd2.ExecuteNonQueryAsync();
+                        }
+
+                        tran.Commit();
+                    }
+                    catch(Exception exp)
+                    {
+#if DEBUG
+                        System.Diagnostics.Debug.WriteLine(exp.Message);
+#endif
+                        tran.Rollback();
+                    }                    
+                }
+            }
+            catch (Exception exp)
+            {
+                System.Diagnostics.Debug.WriteLine(exp.Message);
+                bError = true;
+                strErrMsg = exp.Message;
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+
+            if (bDuplicatedEntry)
+            {
+                return BadRequest("Order already exists: " + nDuplicatedID.ToString());
+            }
+
+            if (bError)
+                return StatusCode(500, strErrMsg);
+
+            vm.ID = nNewID;
+            return new ObjectResult(vm);
         }
 
         // PUT api/financeorder/5
