a1cd05058f67d559c7c529d1794b64d8f41bd414
Alva Chien <alvachien@163.com>
2017-04-14 21:41:51 +0800

    Recent changes

--- src/achihapi/Controllers/FinanceAccountController.cs
+++ src/achihapi/Controllers/FinanceAccountController.cs
@@ -35,7 +35,6 @@ namespace achihapi.Controllers
                     usrName = usrObj.Value;
                     var scopeObj = HIHAPIUtility.GetScopeClaim(this, HIHAPIConstants.FinanceAccountScope);
 
-
                     scopeFilter = HIHAPIUtility.GetScopeSQLFilter(scopeObj.Value, usrName);
                 }
                 catch
