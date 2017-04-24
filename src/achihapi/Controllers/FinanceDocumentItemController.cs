0343fb1b78c18993fe70c35884d8e9cb6073c307
Alva Chien <alvachien@163.com>
2017-04-24 19:11:56 +0800

    Recent changes

---
+++ src/achihapi/Controllers/FinanceDocumentItemController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using achihapi.ViewModels;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace achihapi.Controllers
+{
+    [Route("api/[controller]")]
+    public class FinanceDocumentItemController : Controller
+    {
+        // GET: api/financedocumentitems
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery]Int32? acntid = null, Int32? ccid = null, Int32? ordid = null)
+        {
+            BaseListViewModel<FinanceDocumentUIViewModel> listVMs = new BaseListViewModel<FinanceDocumentUIViewModel>();
+            SqlConnection conn = new SqlConnection(Startup.DBConnectionString);
+            String queryString = "";
+            Boolean bError = false;
+            String strErrMsg = "";
+
+            try
+            {
+                //queryString = SqlUtility.getFinanceDocListQueryString(top, skip);
+
+                await conn.OpenAsync();
+                SqlCommand cmd = new SqlCommand(queryString, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                Int32 nRstBatch = 0;
+                while (reader.HasRows)
+                {
+                    if (nRstBatch == 0)
+                    {
+                        while (reader.Read())
+                        {
+                            listVMs.TotalCount = reader.GetInt32(0);
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                FinanceDocumentUIViewModel avm = new FinanceDocumentUIViewModel();
+                                SqlUtility.FinDocList_DB2VM(reader, avm);
+
+                                listVMs.Add(avm);
+                            }
+                        }
+                    }
+                    ++nRstBatch;
+
+                    reader.NextResult();
+                }
+            }
+            catch (Exception exp)
+            {
+                System.Diagnostics.Debug.WriteLine(exp.Message);
+                bError = true;
+                strErrMsg = exp.Message;
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+
+            if (bError)
+                return StatusCode(500, strErrMsg);
+
+            return new ObjectResult(listVMs);
+        }
+    }
+}
