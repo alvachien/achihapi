83b157e6da6577479e96976ae9333b3f7ab8f137
Alva Chien <alvachien@163.com>
2018-01-31 22:14:54 +0800

    Change the GET to POST

--- src/achihapi/Controllers/RecurEventSimulatorController.cs
+++ src/achihapi/Controllers/RecurEventSimulatorController.cs
@@ -1,46 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using achihapi.ViewModels;
+using achihapi.Utilities;
 
 namespace achihapi.Controllers
 {
     [Produces("application/json")]
     [Route("api/RecurEventSimulator")]
+    [Authorize]
     public class RecurEventSimulatorController : Controller
     {
         // GET: api/RecurEventSimulator
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/RecurEventSimulator/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-        
-        // POST: api/RecurEventSimulator
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Calculate([FromBody]EventGenerationInputViewModel datInput)
         {
-        }
-        
-        // PUT: api/RecurEventSimulator/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-        
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            List<EventGenerationResultViewModel> listRsts = null;
+            listRsts = EventUtility.GenerateEvents(datInput);
+
+            var setting = new Newtonsoft.Json.JsonSerializerSettings
+            {
+                DateFormatString = HIHAPIConstants.DateFormatPattern,
+                ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver()
+            };
+
+            return new JsonResult(listRsts, setting);
         }
     }
 }
