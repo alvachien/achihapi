b28cf7b23340c8952e2f06704b9be03e943bd87d
Alva Chien <alvachien@163.com>
2018-03-13 14:09:06 +0800

    Prepare add event-habit part

---
+++ src/achihapi/Controllers/EventHabitController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace achihapi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/EventHabit")]
+    public class EventHabitController : Controller
+    {
+        // GET: api/EventHabit
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/EventHabit/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/EventHabit
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/EventHabit/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- src/achihapi/ViewModels/EventViewModel.cs
+++ src/achihapi/ViewModels/EventViewModel.cs
@@ -58,4 +58,49 @@ namespace achihapi.ViewModels
         public DateTime EndTimePoint { get; set; }
         public String Name { get; set; }
     }
+
+    public sealed class EventHabitDetail
+    {
+        public Int32 ID { get; set; }
+        public Int32 HabitID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+
+    public sealed class EventHabitViewModel: BaseViewModel
+    {
+        public Int32 ID { get; set; }
+        public Int32 HID { get; set; }
+        [StringLength(50)]
+        public String Name { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+        public RepeatFrequency RptType { get; set; }
+        public Int32 Count { get; set; }
+        public Boolean IsPublic { get; set; }
+        [StringLength(40)]
+        public String Assignee { get; set; }
+
+        public String Tags { get; set; }
+
+        public List<EventHabitDetail> Details { get; private set; }
+
+        public EventHabitViewModel()
+        {
+            this.Details = new List<EventHabitDetail>();
+        }
+    }
+
+    public sealed class EventHabitCheckInViewModel: BaseViewModel
+    {
+        public Int32 ID { get; set; }
+        public Int32 HID { get; set; }
+        public DateTime TranDate { get; set; }
+        public Int32 HabitID { get; set; }
+        public Int32 Score { get; set; }
+        [StringLength(50)]
+        public String Comment { get; set; }
+    }
 }
