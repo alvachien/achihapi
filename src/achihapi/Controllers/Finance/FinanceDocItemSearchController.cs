5d808db2a97c4367bfc7839e29272df96a5bce3d
Alva Chien <alvachien@163.com>
2018-05-02 19:34:07 +0800

    Prepare to add search on document item

---
+++ src/achihapi/Controllers/Finance/FinanceDocItemSearchController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using achihapi.ViewModels;
+using System.Data.SqlClient;
+using achihapi.Utilities;
+
+namespace achihapi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/FinanceDocItemSearch")]
+    [Authorize]
+    public class FinanceDocItemSearchController : Controller
+    {
+        // GET: api/FinanceDocItemSearch
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery]Int32 hid, [FromBody]FinanceDocItemSearchFilterViewModel filters)
+        {
+            if (hid <= 0)
+                return BadRequest("No Home Inputted");
+            if (filters == null || filters.FieldList.Count <= 0)
+                return BadRequest("No filter Inputted");
+
+            // Do the basic checks
+            foreach(var filter in filters.FieldList)
+            {
+                if (!filter.IsValid())
+                    return BadRequest("Invalid filter found");
+            }
+
+            String usrName = String.Empty;
+            if (Startup.UnitTestMode)
+                usrName = UnitTestUtility.UnitTestUser;
+            else
+            {
+                var usrObj = HIHAPIUtility.GetUserClaim(this);
+                usrName = usrObj.Value;
+            }
+            if (String.IsNullOrEmpty(usrName))
+                return BadRequest("User cannot recognize");
+
+            List<FinanceDocumentItemUIViewModel> listVMs = new List<FinanceDocumentItemUIViewModel>();
+            SqlConnection conn = new SqlConnection(Startup.DBConnectionString);
+            String queryString = "";
+            Boolean bError = false;
+            String strErrMsg = "";
+
+            try
+            {
+                String subqueries = "";
+                for(Int32 i = 0; i < filters.FieldList.Count; i ++)
+                {
+                    subqueries += filters.FieldList[i].GenerateSql();
+                    if (i != filters.FieldList.Count - 1)
+                        subqueries += " AND ";
+                }
+                
+                await conn.OpenAsync();
+
+                // Check Home assignment with current user
+                try
+                {
+                    HIHAPIUtility.CheckHIDAssignment(conn, hid, usrName);
+                }
+                catch (Exception exp)
+                {
+                    return BadRequest(exp.Message);
+                }
+
+                SqlCommand cmd = new SqlCommand(queryString, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.HasRows)
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            FinanceDocumentItemViewModel avm = new FinanceDocumentItemViewModel();
+                            SqlUtility.FinDocItemWithBalanceList_DB2VM(reader, avm);
+
+                            listVMs.Add(avm);
+                        }
+                    }
+                    reader.NextResult();
+                }
+            }
+            catch (Exception exp)
+            {
+                System.Diagnostics.Debug.WriteLine(exp.Message);
+                bError = true;
+                strErrMsg = exp.Message;
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                    conn.Dispose();
+                }
+            }
+
+            if (bError)
+                return StatusCode(500, strErrMsg);
+
+            var setting = new Newtonsoft.Json.JsonSerializerSettings
+            {
+                DateFormatString = HIHAPIConstants.DateFormatPattern,
+                ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver()
+            };
+
+            return new JsonResult(listVMs, setting);
+        }
+    }
+}
---
+++ src/achihapi/ViewModels/Finance/FinanceDocItemSearchFilterViewModel.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace achihapi.ViewModels
+{
+    public enum GeneralFilterOperatorEnum
+    {
+        Equal       = 1,
+        NotEqual    = 2,
+        Between     = 3,
+        LargerThan  = 4,
+        LargerEqual = 5,
+        LessThan    = 6,
+        LessEqual   = 7,
+    }
+
+    public class GeneralFilterItem
+    {
+        public string FieldName { get; set; }
+        public GeneralFilterOperatorEnum Operator { get; set; }
+        public string LowValue { get; set; }
+        public string HighValue { get; set; }
+
+        public GeneralFilterItem()
+        {
+            this.Operator = GeneralFilterOperatorEnum.Equal;
+        }
+
+        public Boolean IsValid()
+        {
+            if (String.IsNullOrEmpty(FieldName))
+                return false;
+
+            switch(this.Operator)
+            {
+                case GeneralFilterOperatorEnum.Equal:
+                case GeneralFilterOperatorEnum.NotEqual:
+                case GeneralFilterOperatorEnum.LargerEqual:
+                case GeneralFilterOperatorEnum.LargerThan:
+                case GeneralFilterOperatorEnum.LessEqual:
+                case GeneralFilterOperatorEnum.LessThan:
+                    {
+                        if (String.IsNullOrEmpty(LowValue))
+                            return false;
+                    }
+                    break;
+
+                case GeneralFilterOperatorEnum.Between:
+                    {
+                        if (String.IsNullOrEmpty(LowValue))
+                            return false;
+                        if (String.IsNullOrEmpty(HighValue))
+                            return false;
+                    }
+                    break;
+
+                default:
+                    return false;
+            }
+
+            return true;
+        }
+
+        public string GenerateSql()
+        {
+            if (!this.IsValid())
+                return String.Empty;
+
+            String strRst = "";
+            switch (this.Operator)
+            {
+                case GeneralFilterOperatorEnum.Equal:
+                    {
+                        strRst = FieldName + " = " + LowValue;
+                    }
+                    break;
+
+                case GeneralFilterOperatorEnum.NotEqual:
+                    {
+                        strRst = FieldName + " <> " + LowValue;
+                    }
+                    break;
+
+                case GeneralFilterOperatorEnum.LargerEqual:
+                    {
+                        strRst = FieldName + " >= " + LowValue;
+                    }
+                    break;
+
+                case GeneralFilterOperatorEnum.LargerThan:
+                    {
+                        strRst = FieldName + " > " + LowValue;
+                    }
+                    break;
+
+                case GeneralFilterOperatorEnum.LessEqual:
+                    {
+                        strRst = FieldName + " <= " + LowValue;
+                    }
+                    break;
+
+                case GeneralFilterOperatorEnum.LessThan:
+                    {
+                        strRst = FieldName + " < " + LowValue;
+                    }
+                    break;
+
+                case GeneralFilterOperatorEnum.Between:
+                    {
+                        strRst = FieldName + " >= " + LowValue + " AND " + FieldName + " <= " + HighValue;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+
+            return strRst;
+        }
+    }
+
+    public class FinanceDocItemSearchFilterViewModel
+    {
+        public List<GeneralFilterItem> FieldList { get; private set; }
+
+        public FinanceDocItemSearchFilterViewModel()
+        {
+            this.FieldList = new List<GeneralFilterItem>();
+        }
+    }
+}
