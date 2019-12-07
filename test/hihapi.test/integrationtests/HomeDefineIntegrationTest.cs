7289da636f81c4bf4c307b42982cc308b3723d0e
Alva Chien <alvachien@163.com>
2019-12-07 22:53:06 +0800

    Make integration test passed

--- test/hihapi.test/integrationtests/HomeDefineIntegrationTest.cs
+++ test/hihapi.test/integrationtests/HomeDefineIntegrationTest.cs
@@ -20,11 +20,11 @@ namespace hihapi.test.integrationtests
         public async Task TestCase1_UserA()
         {
             string token = await IdentityServerSetup.Instance.GetAccessTokenForUser(DataSetupUtility.UserA, DataSetupUtility.IntegrationTestPassword);
-            var client = _factory.CreateClient();
-            client.SetBearerToken(token);
+            var clientWithAuth = _factory.CreateClient();
+            clientWithAuth.SetBearerToken(token);
 
             // Step 1. Metadata request
-            var metadata = await client.GetAsync("/api/$metadata");
+            var metadata = await this._client.GetAsync("/api/$metadata");
             Assert.Equal(HttpStatusCode.OK, metadata.StatusCode);
             var content = await metadata.Content.ReadAsStringAsync();
             if (content.Length > 0)
@@ -32,6 +32,15 @@ namespace hihapi.test.integrationtests
                 // How to verify metadata?
                 // TBD.
             }
+
+            // Step 2. Read Home Defines - Non authority case
+            var req1 = await this._client.GetAsync("/api/HomeDefines");
+            Assert.Equal(HttpStatusCode.Unauthorized, req1.StatusCode);
+
+            // Step 3. Read Home Defines - Authority case
+            var req2 = await clientWithAuth.GetAsync("/api/HomeDefines");
+            Assert.True(req2.IsSuccessStatusCode);
+            
         }
     }
 }
