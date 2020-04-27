58e8f07bc11e084c0f27ee21b4b7ea45b7df1a81
Alva Chien <alvachien@163.com>
2020-04-27 20:01:59 +0800

    Make post create work

--- src/hihapi/Controllers/Blog/BlogPostsController.cs
+++ src/hihapi/Controllers/Blog/BlogPostsController.cs
@@ -10,6 +10,9 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using hihapi.Models;
 using hihapi.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using hihapi.Exceptions;
 
 namespace hihapi.Controllers
 {
@@ -32,6 +35,132 @@ namespace hihapi.Controllers
         public SingleResult<BlogPost> Get([FromODataUri] int id)
         {
             return SingleResult.Create(_context.BlogPosts.Where(p => p.ID == id));
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Post([FromBody]BlogPost post)
+        {
+            if (!ModelState.IsValid)
+            {
+                HIHAPIUtility.HandleModalStateError(ModelState);
+            }
+
+            // User
+            string usrName;
+            try
+            {
+                usrName = HIHAPIUtility.GetUserID(this);
+                if (String.IsNullOrEmpty(usrName))
+                {
+                    throw new UnauthorizedAccessException();
+                }
+                if (String.CompareOrdinal(usrName, post.Owner) != 0)
+                {
+                    throw new UnauthorizedAccessException();
+                }
+            }
+            catch
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            post.CreatedAt = DateTime.Now;
+            post.Owner = usrName;
+
+            _context.BlogPosts.Add(post);
+            await _context.SaveChangesAsync();
+
+            return Created(post);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Put([FromODataUri] int key, [FromBody]BlogPost update)
+        {
+            if (!ModelState.IsValid)
+            {
+                HIHAPIUtility.HandleModalStateError(ModelState);
+            }
+
+            if (key != update.ID)
+            {
+                throw new BadRequestException("ID mismatched");
+            }
+
+            // User
+            String usrName = String.Empty;
+            try
+            {
+                usrName = HIHAPIUtility.GetUserID(this);
+                if (String.IsNullOrEmpty(usrName))
+                {
+                    throw new UnauthorizedAccessException();
+                }
+                if (String.CompareOrdinal(usrName, update.Owner) != 0)
+                {
+                    throw new UnauthorizedAccessException();
+                }
+            }
+            catch
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            update.UpdatedAt = DateTime.Now;
+            _context.Entry(update).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException exp)
+            {
+                if (!_context.FinanceAccount.Any(p => p.ID == key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw new DBOperationException(exp.Message);
+                }
+            }
+
+            return Updated(update);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Delete([FromODataUri] int key)
+        {
+            var cc = await _context.BlogPosts.FindAsync(key);
+            if (cc == null)
+            {
+                return NotFound();
+            }
+
+            // User
+            String usrName = String.Empty;
+            try
+            {
+                usrName = HIHAPIUtility.GetUserID(this);
+                if (String.IsNullOrEmpty(usrName))
+                {
+                    throw new UnauthorizedAccessException();
+                }
+                if (String.CompareOrdinal(cc.Owner, usrName) == 0)
+                {
+                    throw new UnauthorizedAccessException();
+                }
+            }
+            catch
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            //if (!cc.IsDeleteAllowed(this._context))
+            //    return BadRequest();
+
+            _context.BlogPosts.Remove(cc);
+            await _context.SaveChangesAsync();
+
+            return StatusCode(204); // HttpStatusCode.NoContent
         }
     }
 }
