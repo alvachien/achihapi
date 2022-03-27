114f358d218c2dd176cebdb3b1c48739c5b1b9e8
Alva Chien <alvachien@live.com>
2022-03-27 20:05:15 +0800

    Upgrade to .Net 6

---
+++ test/hihapi.test/UnitTests/Learn/LearnObjectTest.cs
@@ -0,0 +1,40 @@
+﻿using hihapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace hihapi.unittest.Learn
+{
+    [Collection("HIHAPI_UnitTests#1")]
+    public class LearnObjectTest
+    {
+        [Fact]
+        public void TestCase_Valid1()
+        {
+            LearnObject obj = new LearnObject();
+            obj.Name = "Test";
+            obj.CategoryID = 1;
+            obj.Content = "Test 2";
+            obj.HomeID = 1;
+
+            var isvalid = obj.IsValid(null);
+            Assert.True(isvalid);
+        }
+
+        [Fact]
+        public void TestCase_Invalid_NoHomeID()
+        {
+            LearnObject obj = new LearnObject();
+            obj.Name = "Test";
+            obj.CategoryID = 1;
+            obj.Content = "Test 2";
+            //obj.HomeID = 1;
+
+            var isvalid = obj.IsValid(null);
+            Assert.False(isvalid);
+        }
+    }
+}
